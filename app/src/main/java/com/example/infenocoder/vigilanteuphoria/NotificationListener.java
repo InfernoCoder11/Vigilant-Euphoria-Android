bb77d567d09fa98b529f017f3d40cb359dc46819
Adarsh Menon <38039055+InfernoCoder11@users.noreply.github.com>
2018-06-07 18:15:48 +0530

    Readying NotificationListener

---
+++ app/src/main/java/com/example/infenocoder/vigilanteuphoria/NotificationListener.java
@@ -0,0 +1,16 @@
+package com.example.infenocoder.vigilanteuphoria;
+
+import android.service.notification.NotificationListenerService;
+import android.service.notification.StatusBarNotification;
+import android.util.Log;
+
+public class NotificationListener extends NotificationListenerService {
+
+    public static String TAG = NotificationListener.class.getSimpleName();
+
+    @Override
+    public void onNotificationPosted(StatusBarNotification sbn) {
+
+    }
+
+}
